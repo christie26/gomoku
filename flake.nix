dee9a2d72305a6ca3a8ef630d2d1b0daa843a72e
wendrul <dethomassin.etienne@gmail.com>
2025-08-03 19:28:16 +0200

    Init cargo project

--- flake.nix
+++ flake.nix
@@ -4,15 +4,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ (import rust-overlay) ];
+        };
         pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          tkinter
           # Add your project dependencies here
         ]);
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src" # for rust-analyzer
+            "rust-analyzer"
+            "rustfmt"
+          ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -20,6 +34,7 @@
             pythonEnv
             python3Packages.python-lsp-server
             python3Packages.black
+            rust
           ];
 
         };
