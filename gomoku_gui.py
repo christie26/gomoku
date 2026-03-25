a00b8859d402e5154811add9310ee9490153d03d
christie26 <yoonseo9800@gmail.com>
2026-03-25 11:33:38 +0100

    move util functions to utils.py

--- gomoku_gui.py
+++ gomoku_gui.py
@@ -3,6 +3,7 @@ from tkinter import simpledialog
 from lib_gomoku import Gomoku, MoveResult, get_ai_move
 import argparse
 import time
+from utils import load_and_validate_board, load_board_str, load_history
 
 CELL_SIZE = 35
 BOARD_SIZE = 19
@@ -289,49 +290,6 @@ class GomokuGUI:
             self.draw_possible_stone(y1, x1, "O", score1, selected)
 
         # self.play_one_turn(x, y)
-
-
-def load_and_validate_board(filepath):
-    with open(filepath, "r") as f:
-        lines = [line.strip() for line in f if line.strip()]
-
-    if len(lines) != 19 or any(len(row) != 19 for row in lines):
-        raise ValueError("Board must be 19x19")
-
-    valid_symbols = {".", "X", "O"}
-    board = []
-    count = {"X": 0, "O": 0}
-
-    for row in lines:
-        if any(c not in valid_symbols for c in row):
-            raise ValueError("Board can only contain '.', 'X', or 'O'")
-        for c in row:
-            if c in count:
-                count[c] += 1
-        board.append(list(row))
-
-    current_player = "X"
-
-    return board, current_player
-
-
-def load_board_str(filepath):
-    with open(filepath, "r") as f:
-        content = f.read()
-        return content
-
-
-def load_history(filepath):
-    with open(filepath, "r") as f:
-        content = f.read()
-        historys = content.removeprefix("move history:").strip()
-        history_array = historys.split("->")
-        history_tuples = [
-            (int(array.strip("()").split(",")[0]), int(array.strip("()").split(",")[1]))
-            for array in history_array
-        ]
-
-        return history_tuples
 
 
 if __name__ == "__main__":
