9f7e827690b629cb7bfbaa9ae0b22ad6e1b95182
Yoonseo <yoonseo.lee@padam.io>
2026-08-01 10:40:58 +0200

    use constant for weight

--- lib_gomoku/src/heuristic.rs
+++ lib_gomoku/src/heuristic.rs
@@ -1,21 +1,29 @@
 use crate::{Gomoku, Stone};
 use pyo3::prelude::*;
 
+const CAPTURE_WEIGHT: i32 = 20_000;
+const OPEN_TWO_WEIGHT: i32 = 1;
+const FREE_THREE_WEIGHT: i32 = 1_000;
+const OPEN_THREE_WEIGHT: i32 = 100;
+const BLOCK_FOUR_WEIGHT: i32 = 1;
+const OPEN_FOUR_WEIGHT: i32 = 40_000;
+const FIVE_ROW_WEIGHT: i32 = 80_001;
+
 fn evaluate_player(state: &Gomoku, player: &Stone) -> i32 {
   
-    let capture_score = state.capture_count.get(player).unwrap_or(&0) * 20000;
+    let capture_score = state.capture_count.get(player).unwrap_or(&0) * CAPTURE_WEIGHT;
     
-    let open_two_score = state.open_two.get(player).map_or(0, |v| v.len() as i32) * 1;
+    let open_two_score = state.open_two.get(player).map_or(0, |v| v.len() as i32) * OPEN_TWO_WEIGHT;
     
     let free_three_count = state.free_three.get(player).map_or(0, |v| v.len() as i32);
-    let free_three_score = free_three_count * 1000;
+    let free_three_score = free_three_count * FREE_THREE_WEIGHT;
     let open_three_score =
-        (state.open_three.get(player).map_or(0, |v| v.len() as i32) - free_three_count) * 100;
+        (state.open_three.get(player).map_or(0, |v| v.len() as i32) - free_three_count) * OPEN_THREE_WEIGHT;
 
-    let block_four_score = state.block_four.get(player).map_or(0, |v| v.len() as i32) * 1;
+    let block_four_score = state.block_four.get(player).map_or(0, |v| v.len() as i32) * BLOCK_FOUR_WEIGHT;
 
-    let open_four_score = state.open_four.get(player).map_or(0, |v| v.len() as i32) * 40000;
-    let five_row_score = state.five_row.get(player).map_or(0, |v| v.len() as i32) * 80001;
+    let open_four_score = state.open_four.get(player).map_or(0, |v| v.len() as i32) * OPEN_FOUR_WEIGHT;
+    let five_row_score = state.five_row.get(player).map_or(0, |v| v.len() as i32) * FIVE_ROW_WEIGHT;
 
     // println!("[Player {}] Capture: {}, Open 2: {}, Open 3: {}, \
     // Block 4: {}, Open 4: {}, Free 3: {}, 5: {}",
