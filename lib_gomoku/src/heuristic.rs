6b61a8a103cbc3e6174199043821b1a4a1362797
Yoonseo <yoonseo.lee@padam.io>
2026-08-01 10:39:39 +0200

    change order of line

--- lib_gomoku/src/heuristic.rs
+++ lib_gomoku/src/heuristic.rs
@@ -2,11 +2,12 @@ use crate::{Gomoku, Stone};
 use pyo3::prelude::*;
 
 fn evaluate_player(state: &Gomoku, player: &Stone) -> i32 {
-    let free_three_count = state.free_three.get(player).map_or(0, |v| v.len() as i32);
-
+  
     let capture_score = state.capture_count.get(player).unwrap_or(&0) * 20000;
+    
     let open_two_score = state.open_two.get(player).map_or(0, |v| v.len() as i32) * 1;
-
+    
+    let free_three_count = state.free_three.get(player).map_or(0, |v| v.len() as i32);
     let free_three_score = free_three_count * 1000;
     let open_three_score =
         (state.open_three.get(player).map_or(0, |v| v.len() as i32) - free_three_count) * 100;
