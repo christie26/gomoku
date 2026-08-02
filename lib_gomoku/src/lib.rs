d0bdc2b64305d6c31d05e96b8113c81060f9bd57
Yoonseo <yoonseo.lee@padam.io>
2026-08-02 21:59:26 +0200

    remove handle_move_simple_ruleset

--- lib_gomoku/src/lib.rs
+++ lib_gomoku/src/lib.rs
@@ -246,6 +246,7 @@ impl Gomoku {
     }
 
     fn get_free_threes_from_move(&self, x0: i32, y0: i32) -> Vec<Pattern> {
+      // TODO we can add caching here
         let mut new_free_threes = Vec::new();
         let directions = [(1, -1), (1, 0), (1, 1), (0, 1)];
 
@@ -644,30 +645,6 @@ impl Gomoku {
         //     return MoveResult::DoubleThree;
         // }
         MoveResult::Valid
-    }
-
-    pub fn handle_move_simple_ruleset(&mut self, x: i32, y: i32) -> (MoveResult, i32) {
-        let result = self.is_valid_move_simple_ruleset(x, y);
-        let mut capture_count = 0;
-
-        if result == MoveResult::Valid {
-            self.current_move = Some((x, y));
-            self.move_count += 1;
-            self.board[x as usize][y as usize] = self.current_player.clone();
-
-            // self.remove_free_three(x, y, &self.opponent_player.clone());
-            self.remove_opens(x, y);
-
-            // self.add_free_threes(
-            //     self.get_free_threes_from_move(x, y),
-            //     &self.current_player.clone(),
-            // );
-            self.add_opens_from_move(x, y);
-
-            // capture_count = self.capture_center(x, y);
-        }
-
-        (result, capture_count)
     }
 
     pub fn handle_move(&mut self, x: i32, y: i32)
