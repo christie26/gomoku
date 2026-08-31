1c41735f57ff3d5841e231b8ac95faeada79a179
ede-thom <ede-thom@f6r3s15.paris.42.school>
2026-08-31 17:15:43 +0200

    make makefile work properly

--- src/board_canvas.py
+++ src/board_canvas.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from lib_gomoku import MoveResult
-from src.screen_constant import CELL_SIZE, LABEL_PADDING, BOARD_SIZE, PADDING, LIGHT_BACKGROUND, SELECT_BACKGROUND, BORDER_COLOR, LABEL_FONT, NAME_FONT
+from screen_constant import CELL_SIZE, LABEL_PADDING, BOARD_SIZE, PADDING, LIGHT_BACKGROUND, SELECT_BACKGROUND, BORDER_COLOR, LABEL_FONT, NAME_FONT
 
 class BoardCanvas:
     def __init__(self, parent, canvas_size, on_click):
